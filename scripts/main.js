0ffa1ab6a39fca8464884c59e1af26f3a480d421
Adnan-Gading <152518116+Adnan-Gading@users.noreply.github.com>
2023-12-02 23:31:08 +0700

    Portfolio

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// Toogle & Responsive
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
